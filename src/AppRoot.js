996f14b4ee915d15ff00dc305b95312516d0e680
Irakli Kobalia <qobalia92@gmail.com>
2017-10-20 01:56:28 +0400

    feat: Beautify navigation

--- src/AppRoot.js
+++ src/AppRoot.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import { View } from 'react-native'
+import { View, Platform } from 'react-native'
 import { createStore } from 'redux'
 import { StackNavigator, TabNavigator } from 'react-navigation'
 import reducer from './reducers/index'
@@ -9,20 +9,36 @@ import NewDeck from './components/deck/NewDeck'
 import IndividualDeck from './components/deck/IndividualDeck'
 import NewCard from './components/card/NewCard'
 import Quiz from './components/quiz/Quiz'
-
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { white, black, yellow, gray } from './utils/colors'
 
 const Tabs = TabNavigator({
         DeckList: {
             screen: DeckList,
             navigationOptions: {
-                tabBarLabel: 'Decks'
+                tabBarLabel: 'Decks',
+                tabBarIcon: Platform.OS === 'ios' && (() => <Ionicons name='ios-list' size={30} color={black} />),
             },
         },
         NewDeck: {
             screen: NewDeck,
             navigationOptions: {
-                tabBarLabel: 'New Deck'
+                tabBarLabel: 'New Deck',
+                tabBarIcon: Platform.OS === 'ios' && (() => <Ionicons name='ios-add' size={30} color={black} />)
             },
+        }
+    },
+    {
+        animationEnabled: true,
+        tabBarOptions: {
+            inactiveTintColor: black,
+            activeTintColor: yellow,
+            labelStyle: {
+                fontSize: 12,
+            },
+            style: {
+                backgroundColor: white,
+            }
         }
     }
 )
@@ -30,26 +46,41 @@ const Tabs = TabNavigator({
 const AppNavigator = StackNavigator({
     Home: {
         screen: Tabs,
-        navigationOptions: {title: 'Home'},
+        navigationOptions: {
+            title: 'Home',
+            headerStyle: {
+                backgroundColor: black
+            },
+            headerTintColor: white
+        },
     },
     IndividualDeck: {
         screen: IndividualDeck,
         navigationOptions: {
-            headerTintColor: '#7c7e80'
+            headerStyle: {
+                backgroundColor: black
+            },
+            headerTintColor: white
         }
     },
     NewCard: {
         screen: NewCard,
         navigationOptions: {
             title: 'Add Card',
-            headerTintColor: '#7c7e80'
+            headerStyle: {
+                backgroundColor: black
+            },
+            headerTintColor: white
         }
     },
     Quiz: {
         screen: Quiz,
         navigationOptions: {
             title: 'Quiz',
-            headerTintColor: '#7c7e80'
+            headerStyle: {
+                backgroundColor: black
+            },
+            headerTintColor: white
         }
     }
 })
